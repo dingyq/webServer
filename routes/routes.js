21bc7c4079e4538ba494815d2d4b72986b19368c
dingyq <dingyq23@qq.com>
2015-08-19 21:39:22 +0800

    url decode

--- routes/routes.js
+++ routes/routes.js
@@ -9,7 +9,7 @@ module.exports = function(rest){
 	rest.get('/bp:gamemap', gamemap.getMap);
 	// rest.post('/gamgaemap', gamemap.getMap);
 	rest.get('/bp:wxtoken', function(req, res, cb) {
-		var url = req.params.url || 'http://995078.com';
+		var url = decodeURIComponent(req.params.url);
 		wxShare.getWxShareBasicData(function(result){
 			cb(null, result);
 		}, url);
